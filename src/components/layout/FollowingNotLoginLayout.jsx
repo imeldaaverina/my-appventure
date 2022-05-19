8cf87ad2a9f84365ffb8e939209b7063682ec203
imeldaaverin <22imelda.averina@gmail.com>
2022-05-19 13:38:10 +0700

    revisi tag img

--- src/components/layout/FollowingNotLoginLayout.jsx
+++ src/components/layout/FollowingNotLoginLayout.jsx
@@ -3,6 +3,7 @@ import Footer from "./elements/Footer";
 import Jumbotron from "./elements/Jumbotron";
 import NavbarFollowingNotLogin from "./elements/NavbarFollowingNotLogin";
 
+
 const FollowingNotLoginLayout = () => {
 
   return (
@@ -13,7 +14,7 @@ const FollowingNotLoginLayout = () => {
           <NavbarFollowingNotLogin/>
           <div className="pt-20 text-center">
             <div className="flex justify-center">
-            <img src="/Feeling sorry-pana 1.svg" alt=""/>
+            <Image src="/Feeling sorry-pana 1.svg" alt=""/>
             </div>
             <div className="pt-5 text-lg">
               <p>Anda belum mengikuti siapapun</p>
